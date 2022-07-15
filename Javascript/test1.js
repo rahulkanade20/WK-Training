4a3a59591a124a1d09d3aed0c7d46073c6ef3272
Kanade <Rahul.Kanade@wkfs-frc.local>
2022-07-15 17:54:05 +0530

    commit

---
+++ Javascript/test1.js
@@ -0,0 +1,45 @@
+// chrome engine acts as a interpreter for javascript
+// at heart of node js is this chrome engine which interpretes javascript and other things added like 
+// npm which consists of large 3rd party library for node js javascript
+// wsl windows subsystem for linux
+// python and javascript are not type safe languages ... to solve this javascript has come up with type script
+// bigint in javascript 2^53-1
+console.log("hello")
+
+x = 5;
+y = 10;
+console.log(x+y);
+console.log(typeof x);
+
+var person = {
+
+    name: "abc",
+    age: 2091
+}
+
+console.log(person)
+console.log(person.name)
+console.log(person.age)
+console.log(person["name"])
+console.log(person["age"])
+
+if(true) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+
+// loops in javascript
+// different string methods in javascript
+// .length to find length of the string
+
+
+// string template literals
+
+name1 = "def"
+
+let sen = `The name is ${name1}`
+
+console.log(sen)
